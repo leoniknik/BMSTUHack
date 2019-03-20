e96cf7993cc5b50ee1f3f51787afe7fd8dcb3bf1
Evtuhov <Evtuhov@binbank.ru>
2019-03-20 23:11:54 +0300

    no message

---
+++ RxProjectBMSTUHack/RxProjectBMSTUHack/Classes/Presentation/Menu/View/Meal/MealCell.swift
@@ -0,0 +1,16 @@
+//
+//  MealCell.swift
+//  RxProjectBMSTUHack
+//
+//  Created by Aleksander Evtuhov on 20/03/2019.
+//  Copyright © 2019 Кирилл Володин. All rights reserved.
+//
+
+import UIKit
+
+class MealCell: UICollectionViewCell {
+    @IBOutlet weak var mealImage: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    
+}
--- RxProjectBMSTUHack/RxProjectBMSTUHack/Classes/Presentation/Menu/ViewController/MealListViewController.swift
+++ RxProjectBMSTUHack/RxProjectBMSTUHack/Classes/Presentation/Menu/ViewController/MealListViewController.swift
@@ -9,8 +9,34 @@
 import UIKit
 
 final class MealListViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var meals = [Meal]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.register(MealCell.self, forCellWithReuseIdentifier: "\(MealCell.self)")
     }
+}
+
+extension MealListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return meals.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(MealCell.self)", for: indexPath) as? MealCell else { return UICollectionViewCell() }
+        let meal = meals[indexPath.item]
+        cell.mealImage.image = meal.image
+        cell.title.text = meal.name
+        cell.price.text = "\(meal.price) Руб"
+        return cell
+    }
+    
+    
+}
+
+extension MealListViewController: UICollectionViewDelegate {
+    
 }
