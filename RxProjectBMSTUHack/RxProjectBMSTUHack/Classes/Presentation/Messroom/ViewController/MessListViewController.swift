eda494af77eaa83627c2b587a21f866d75479862
VolodinK <VolodinK@binbank.ru>
2019-03-20 19:02:55 +0300

    initial commit

---
+++ RxProjectBMSTUHack/RxProjectBMSTUHack/Classes/Presentation/Messroom/ViewController/MessListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MessListViewController.swift
+//  RxProjectBMSTUHack
+//
+//  Created by Кирилл Володин on 20/03/2019.
+//  Copyright © 2019 Кирилл Володин. All rights reserved.
+//
+
+import UIKit
+
+class MessListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
